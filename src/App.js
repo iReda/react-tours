8084dcb6338c472b0816442f93e8db4c61891ba3
AbdelRahman Reda <veguslive@gmail.com>
2021-04-01 00:09:56 +0200

    add loading component and fetching from API to get tours data

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [toursArr, setToursArr] = useState([]);
+  const fetchTours = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setToursArr(tours);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours(url);
+  }, []);
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <Tours />
+      </main>
+    );
+  }
+}
+
+export default App;
